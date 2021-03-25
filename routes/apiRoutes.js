2282955e4c7a961bcaa06dcd99919330db2430a7
aqrit <aqrit@aqr.local>
2021-03-25 15:34:34 +1100

    completed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,5 +12,37 @@ module.exports = router => {
         });
 
     });
+// create a new workout
+    router.post("/api/workouts", (req, res) => {
+        Workout.create({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err)
+        });
+    });
 
+// add exercise to a previously created workout
+    router.put("/api/workouts/:id", (req, res) => {
+        Workout.findOneAndUpdate(
+            {
+                _id: req.params.id
+            },
+            { $push: { exercises: req.body } },
+            { new: true })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+    });
+
+
+// view combined weight of multiple exercises
+    router.get("/api/workouts/range", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
 };
